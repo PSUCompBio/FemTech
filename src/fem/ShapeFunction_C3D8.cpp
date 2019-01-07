eb191a7932486db638f3d64d67a92f8197a4a724
AbdulrahmanYasser95 <38386819+AbdulrahmanYasser95@users.noreply.github.com>
2019-01-07 03:36:41 +0200

    Update ShapeFunction_C3D8.cpp
    7/1/19
    still incomplete

--- src/fem/ShapeFunction_C3D8.cpp
+++ src/fem/ShapeFunction_C3D8.cpp
@@ -1,5 +1,5 @@
 #include "digitalbrain.h"
-void ShapeFunction_C3D8(){
+void ShapeFunction_C3D8(/*ss[], xsj, shp[], xl[], ndm*/){
    printf("Rank %d: Shape Functions for C3D8 elements!!\n", world_rank);
 
    /*!$Id:$
@@ -30,82 +30,86 @@ void ShapeFunction_C3D8(){
 
 	   implicit  none*/
 
-	   integer   ndm, i, j, k
-	   real * 8    rxsj, xsj, ap1, am1, ap2, am2, ap3, am3, c1, c2, c3
+	   //integer   ndm, i, j, k
+   //int   ndm, i, j, k;
+	   //real * 8    rxsj, xsj, ap1, am1, ap2, am2, ap3, am3, c1, c2, c3
+   //double    rxsj, xsj, ap1, am1, ap2, am2, ap3, am3, c1, c2, c3;
 
-	   real * 8    ss(3), shp(4, 8), xl(ndm, 8), xs(3, 3), ad(3, 3)
+   //real * 8    ss(3), shp(4, 8), xl(ndm, 8), xs(3, 3), ad(3, 3)
+	   //double    ss[3-1], shp[4-1, 8-1], xl[ndm-1, 8-1], xs[3-1, 3-1], ad[3-1, 3-1];
 
-	   save
+	   //save
 
 	   //!Compute shape functions and their natural coord.derivatives
 
-	   ap1 = 1.0d0 + ss(1)
-	   am1 = 1.0d0 - ss(1)
-	   ap2 = 1.0d0 + ss(2)
-	   am2 = 1.0d0 - ss(2)
-	   ap3 = 1.0d0 + ss(3)
-	   am3 = 1.0d0 - ss(3)
+	   ap1 = 1.0d0 + ss[1 - 1];
+	   am1 = 1.0d0 - ss[1 - 1];
+	   ap2 = 1.0d0 + ss[2 - 1];
+	   am2 = 1.0d0 - ss[2 - 1];
+	   ap3 = 1.0d0 + ss[3 - 1];
+	   am3 = 1.0d0 - ss[3 - 1];
 
 	   //!Compute for (-, -) values
 
-	   c1 = 0.125d0*am1*am2
-	   c2 = 0.125d0*am2*am3
-	   c3 = 0.125d0*am1*am3
-	   shp(1, 1) = -c2
-	   shp(1, 2) = c2
-	   shp(2, 1) = -c3
-	   shp(2, 4) = c3
-	   shp(3, 1) = -c1
-	   shp(3, 5) = c1
-	   shp(4, 1) = c1 * am3
-	   shp(4, 5) = c1 * ap3
+	   c1 = 0.125*am1*am2;
+	   c2 = 0.125*am2*am3;
+	   c3 = 0.125*am1*am3;
+	   shp[1 - 1, 1 - 1] = -c2;
+	   shp[1 - 1, 2 - 1] = c2;
+	   shp[2 - 1, 1 - 1] = -c3;
+	   shp[2 - 1, 4 - 1] = c3;
+	   shp[3 - 1, 1 - 1] = -c1;
+	   shp[3 - 1, 5 - 1] = c1;
+	   shp[4 - 1, 1 - 1] = c1 * am3;
+	   shp[4 - 1, 5 - 1] = c1 * ap3;
 
 	   //!Compute for (+, +) values
 
-	   c1 = 0.125d0*ap1*ap2
-	   c2 = 0.125d0*ap2*ap3
-	   c3 = 0.125d0*ap1*ap3
-	   shp(1, 8) = -c2
-	   shp(1, 7) = c2
-	   shp(2, 6) = -c3
-	   shp(2, 7) = c3
-	   shp(3, 3) = -c1
-	   shp(3, 7) = c1
-	   shp(4, 3) = c1 * am3
-	   shp(4, 7) = c1 * ap3
+	   c1 = 0.125*ap1*ap2;
+	   c2 = 0.125*ap2*ap3;
+	   c3 = 0.125*ap1*ap3;
+	   shp[1-1, 8 - 1] = -c2;
+	   shp[1 - 1, 7 - 1] = c2;
+	   shp[2 - 1, 6 - 1] = -c3;
+	   shp[2 - 1, 7 - 1] = c3;
+	   shp[3 - 1, 3 - 1] = -c1;
+	   shp[3 - 1, 7 - 1] = c1;
+	   shp[4 - 1, 3 - 1] = c1 * am3;
+	   shp[4 - 1, 7 - 1] = c1 * ap3;
 
 	   //!Compute for (-, +) values
 
-	   c1 = 0.125d0*am1*ap2
-	   c2 = 0.125d0*am2*ap3
-	   c3 = 0.125d0*am1*ap3
-	   shp(1, 5) = -c2
-	   shp(1, 6) = c2
-	   shp(2, 5) = -c3
-	   shp(2, 8) = c3
-	   shp(3, 4) = -c1
-	   shp(3, 8) = c1
-	   shp(4, 4) = c1 * am3
-	   shp(4, 8) = c1 * ap3
+	   c1 = 0.125*am1*ap2;
+	   c2 = 0.125*am2*ap3;
+	   c3 = 0.125*am1*ap3;
+	   shp[1 - 1, 5 - 1] = -c2;
+	   shp[1 - 1, 6 - 1] = c2;
+	   shp[2 - 1, 5 - 1] = -c3;
+	   shp[2 - 1, 8 - 1] = c3;
+	   shp[3 - 1, 4 - 1] = -c1;
+	   shp[3 - 1, 8 - 1] = c1;
+	   shp[4 - 1, 4 - 1] = c1 * am3;
+	   shp[4 - 1, 8 - 1] = c1 * ap3;
 
 	   //!Compute for (+, -) values
 
-	   c1 = 0.125d0*ap1*am2
-	   c2 = 0.125d0*ap2*am3
-	   c3 = 0.125d0*ap1*am3
-	   shp(1, 4) = -c2
-	   shp(1, 3) = c2
-	   shp(2, 2) = -c3
-	   shp(2, 3) = c3
-	   shp(3, 2) = -c1
-	   shp(3, 6) = c1
-	   shp(4, 2) = c1 * am3
-	   shp(4, 6) = c1 * ap3
-	   if (ndm.lt.3) return
+	   c1 = 0.125*ap1*am2;
+	   c2 = 0.125*ap2*am3;
+	   c3 = 0.125*ap1*am3;
+	   shp[1 - 1, 4 - 1] = -c2;
+	   shp[1 - 1, 3 - 1] = c2;
+	   shp[2 - 1, 2 - 1] = -c3;
+	   shp[2 - 1, 3 - 1] = c3;
+	   shp[3 - 1, 2 - 1] = -c1;
+	   shp[3 - 1, 6 - 1] = c1;
+	   shp[4 - 1, 2 - 1] = c1 * am3;
+	   shp[4 - 1, 6 - 1] = c1 * ap3;
+	   if (ndm < 3) return;
 
 		   //!Compute jacobian transformation
 
-		   do j = 1, 3
+		   //do j = 1, 3
+			for(int j = 0;j< 3;j++){}
 			   xs(j, 1) = (xl(j, 2) - xl(j, 1))*shp(1, 2)
 			   &          +(xl(j, 3) - xl(j, 4))*shp(1, 3)
 			   &          +(xl(j, 6) - xl(j, 5))*shp(1, 6)
