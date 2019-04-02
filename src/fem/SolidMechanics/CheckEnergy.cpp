49b418e28e19dcf2fdde36c6b26ffa40378578c7
Anil Das P V <anil.das.pv+git@gmail.com>
2019-04-02 19:47:58 +0530

    With wave speed compute for time step

--- src/fem/SolidMechanics/CheckEnergy.cpp
+++ src/fem/SolidMechanics/CheckEnergy.cpp
@@ -5,14 +5,18 @@ void CheckEnergy(void) {
   static double Wext_n = 0.0;
   double sum = 0.0;
   for(int i = 0; i < nnodes*ndim; ++i) {
-    sum += (displacements[i]-displacements_prev[i])*(fi[i]+fi_prev[i]);
+    // if(!boundary[i]) {
+      sum += (displacements[i]-displacements_prev[i])*(fi[i]+fi_prev[i]);
+    // }
   }
   sum *= 0.5;
   Wint_n += sum;
   printf("Internal Work : %.24f\n", Wint_n);
   double WKE = 0.0;
   for(int i = 0; i < nnodes*ndim; ++i) {
-    WKE += mass[i]*velocities[i]*velocities[i];
+    // if(!boundary[i]) {
+      WKE += mass[i]*velocities[i]*velocities[i];
+    // }
   }
   WKE *= 0.5;
   printf("Kinetic Energy : %.24f\n", WKE);
--- src/timestep/CalculateTimeStep.cpp
+++ src/timestep/CalculateTimeStep.cpp
@@ -4,16 +4,22 @@
 /** For a single element - this function calculates the volume of the element
     and calculates the critical time step based on the wave speed.*/
 
-double CalculateTimeStep(int e){
-
+double CalculateTimeStep(int e) {
 	double dt;
+  const double dtMax = 0.1;
  	// characteristic element length
   double le = 1.0;
 	//wave speed of material
-	double ce = sqrt(262.5/1000.);
+  double mu = properties[MAXMATPARAMS * e + 1];
+  double lambda = properties[MAXMATPARAMS * e + 2];
+  double rho = properties[MAXMATPARAMS * e + 0];
+  double nu = 0.5*lambda/(lambda+mu);
+  // ce calculated using dilatatoin wave speed
+	double ce = sqrt(lambda*(1.0/nu-1.0)/rho);
 	dt = le/ce;
-
-	dt = 1e-4;
-
+  if (dt > dtMax) {
+    dt = dtMax;
+  }
+  printf("Time Step : %.24f\n", dt);
 	return dt;
 }
