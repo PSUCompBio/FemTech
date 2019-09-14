37045910425a09d887881b911910d5686939f8d8
Anil Das P V <anil.das.pv+git@gmail.com>
2019-09-15 02:34:04 +0530

    Skipped elements with rigid body motion from min dt computation

--- src/timestep/StableTimeStep.cpp
+++ src/timestep/StableTimeStep.cpp
@@ -6,13 +6,31 @@ double StableTimeStep() {
 
 	double dt;
 	double dtMin = huge;
+  bool isNotRigid;
+  // int minElementID;
+  // int elemSkipped = 0;
 
 	for(int i=0;i<nelements;i++){
- 		dt = CalculateTimeStep(i);
-		if (dt < dtMin){
-			dtMin = dt;
-		}
+    isNotRigid = false;
+    for (int j = eptr[i]; j < eptr[i+1]; ++j) {
+      if (!boundary[connectivity[j]]) {
+        isNotRigid = true;
+        break;
+      }
+    }
+    if (isNotRigid) {
+      dt = CalculateTimeStep(i);
+      if (dt < dtMin){
+        dtMin = dt;
+        // minElementID = i;
+      }
+    } 
+    // else {
+    //   elemSkipped = elemSkipped + 1;
+    // }
 	}
+  // printf("INFO(%d): Minimum dt from element %d\n", world_rank, minElementID);
+  // printf("INFO(%d): Elements skipped %d\n", world_rank, elemSkipped);
   MPI_Allreduce(MPI_IN_PLACE, &dtMin, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
 
   if(dtMin < FailureTimeStep){
