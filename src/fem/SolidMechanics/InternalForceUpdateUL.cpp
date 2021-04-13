22c7b43b17416ebf4f3b0fab052a7ae00ca1dcf4
RitikaMenghani <36120424+RitikaMenghani@users.noreply.github.com>
2021-04-13 10:05:23 -0400

    Update InternalForceUpdateUL.cpp
    updated to add dynamicvisco

--- src/fem/SolidMechanics/InternalForceUpdateUL.cpp
+++ src/fem/SolidMechanics/InternalForceUpdateUL.cpp
@@ -9,27 +9,36 @@ void InternalForceUpdateUL(int e, int gp, double *force) {
 
   const unsigned int wIndex = gpPtr[e]+gp;
   const double preFactor = gaussWeights[wIndex]*J_Xi;
-
-  for (unsigned int I = 0; I < nShapeFunc; ++I) {
+  double p, p1, p2;
+  double rho = properties[MAXMATPARAMS * pid[e] + 0];
+  double mu = properties[MAXMATPARAMS * pid[e] + 1];
+  double lambda = properties[MAXMATPARAMS * pid[e] + 2];
+  double ce = sqrt((lambda+2*mu)/rho);
+  double le = CalculateCharacteristicLength(e);
+  double strate = 0.0;
+  double b1=0.06;
+  double b2=1.2;
+  for(int i = eptr[e], I = 0; i < eptr[e+1]; ++i, ++I)
+  {
+    const unsigned int indexShp = indexStart + I * ndim;
+    strate += velocities_half[ndim*connectivity[i]+0]*(dshp[indexShp]*F_XiInverse[0] + dshp[indexShp+1]*F_XiInverse[3] + dshp[indexShp+2]*F_XiInverse[6]) + velocities_half[ndim*connectivity[i]+1]*(dshp[indexShp]*F_XiInverse[1] + dshp[indexShp+1]*F_XiInverse[4] + dshp[indexShp +2]*F_XiInverse[7]) + velocities_half[ndim*connectivity[i]+2]*(dshp[indexShp]*F_XiInverse[2] + dshp[indexShp+1]*F_XiInverse[5] + dshp[indexShp +2]*F_XiInverse[8]);
+    }
+  p1 = b1*rho*ce*le*strate;
+  p2 = rho*pow((b2*le),2)*strate*std::min(0.0, strate);
+  p = p1 + p2;
+    for (unsigned int I = 0; I < nShapeFunc; ++I) {
     const unsigned int indexShp = indexStart + I * ndim;
     // Compute dN_I/dx_j = (dN_I/d_Xi)^T F_Xi^-{-1}
-    const double dN_Ibydx = dshp[indexShp]*F_XiInverse[0] + 
+    const double dN_Ibydx = dshp[indexShp]*F_XiInverse[0] +
       dshp[indexShp+1]*F_XiInverse[3] + dshp[indexShp+2]*F_XiInverse[6];
-    const double dN_Ibydy = dshp[indexShp]*F_XiInverse[1] + 
+    const double dN_Ibydy = dshp[indexShp]*F_XiInverse[1] +
       dshp[indexShp+1]*F_XiInverse[4] + dshp[indexShp +2]*F_XiInverse[7];
-    const double dN_Ibydz = dshp[indexShp]*F_XiInverse[2] + 
+    const double dN_Ibydz = dshp[indexShp]*F_XiInverse[2] +
       dshp[indexShp+1]*F_XiInverse[5] + dshp[indexShp +2]*F_XiInverse[8];
-
     // Compute f_iI = dN_I/dx_j*sigma_{ji}
-    // Get location of Cauchy values
-    double * sigma_nLocal = &(sigma_n[sigmaptr[e]+6*gp]);
-
-    const double fxI = dN_Ibydx*sigma_nLocal[0]+\
-                       dN_Ibydy*sigma_nLocal[5]+dN_Ibydz*sigma_nLocal[4];
-    const double fyI = dN_Ibydx*sigma_nLocal[5]+\
-                       dN_Ibydy*sigma_nLocal[1]+dN_Ibydz*sigma_nLocal[3];
-    const double fzI = dN_Ibydx*sigma_nLocal[4]+\
-                       dN_Ibydy*sigma_nLocal[3]+dN_Ibydz*sigma_nLocal[2];
+    const double fxI = dN_Ibydx*(sigma_n[0]+p)+dN_Ibydy*(sigma_n[5]+p)+dN_Ibydz*(sigma_n[4]+p);
+    const double fyI = dN_Ibydx*(sigma_n[5]+p)+dN_Ibydy*(sigma_n[1]+p)+dN_Ibydz*(sigma_n[3]+p);
+    const double fzI = dN_Ibydx*(sigma_n[4]+p)+dN_Ibydy*(sigma_n[3]+p)+dN_Ibydz*(sigma_n[2]+p);
     const unsigned int forceIndex = I*ndim;
     // Add to fint
     force[forceIndex] += preFactor*fxI;
@@ -38,3 +47,4 @@ void InternalForceUpdateUL(int e, int gp, double *force) {
   }
 	return;
 }
+
