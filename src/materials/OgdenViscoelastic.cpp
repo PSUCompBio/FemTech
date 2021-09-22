7eeb46edbd37d4f4bc16b09c999d6fcdf7e8f4a4
RitikaMenghani <ritikarmenghani@gmail.com>
2021-09-22 12:17:47 -0400

    added all hardy files

--- src/materials/OgdenViscoelastic.cpp
+++ src/materials/OgdenViscoelastic.cpp
@@ -123,7 +123,7 @@ void OgdenViscoelastic(int e, int gp) {
     preFactor /= cEigenValue[i];
     dyadic(&Cmat[3*i], preFactor, devS0np1);
   }
-  FILE_LOG_SINGLE(WARNING, "Trace of devS0np1 = %15.9f", devS0np1[0]+devS0np1[4]+devS0np1[8]);
+ // FILE_LOG_SINGLE(WARNING, "Trace of devS0np1 = %15.9f", devS0np1[0]+devS0np1[4]+devS0np1[8]);
 
   // Access histroy dependence
   double *Sdev = mat4;
@@ -161,7 +161,7 @@ void OgdenViscoelastic(int e, int gp) {
 
   // Compute deviatoric part of sigmaTilde and add the isochoric part to obtain
   // sigma
-  FILE_LOG_SINGLE(WARNING, "Trace of sigma = %15.9f", sigma[0]+sigma[4]+sigma[8]);
+ // FILE_LOG_SINGLE(WARNING, "Trace of sigma = %15.9f", sigma[0]+sigma[4]+sigma[8]);
   const double volum = invJ*K*(J-1.0); 
   // Compute sigma
   double* sigma_e = sigma;
